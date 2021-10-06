1815540d921a7a5b52121eeab8964fda10c687ac
siva sankara prasad <psiva.iiit@gmail.com>
2021-10-07 01:31:36 +0530

    createTask function has been added

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.7;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    constructor() {
+        createTask("Mapping, functions learning");
+    }
+
+    mapping(uint256 => Task) public Tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        Tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
